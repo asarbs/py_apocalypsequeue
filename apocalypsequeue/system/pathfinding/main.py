6d3c5bbaf43e15a891e9123f1f6bc487c0940107
asar <asar.bs@gmail.com>
2021-02-10 21:57:02 +0100

    Dijkstra's algorithm implementation for path finding

--- apocalypsequeue/system/pathfinding/main.py
+++ apocalypsequeue/system/pathfinding/main.py
@@ -1,8 +1,11 @@
 import pygame
 import math
+import pprint
+import operator
 from Vector import Vector
 RED = (255, 0, 0)
-
+GREEN = (0,255,0)
+PINK = (248,191,223)
 
 class NavGraphNode(pygame.sprite.Sprite):
     count = 1
@@ -20,6 +23,7 @@ class NavGraphNode(pygame.sprite.Sprite):
         self.rect = pygame.Rect(position.getTuple(), (5, 5))
         self.__id = NavGraphNode.count
         self.edge_list = []
+        self.__color = RED
         NavGraphNode.count += 1
 
     def __hash__(self):
@@ -32,26 +36,40 @@ class NavGraphNode(pygame.sprite.Sprite):
         weight = (self.__position - neighbor.__position).getLength()
         self.edge_list.append(NavGraphNode.Edge(neighbor, weight))
 
+    def get_neighbors(self):
+        return self.edge_list
+
+    def mark(self):
+        self.__color = GREEN
+
+    def mark_path(self):
+        self.__color = PINK
+
     def draw(self, screen):
-        pygame.draw.circle(screen, RED, self.rect.center, 2)
+        pygame.draw.circle(screen, self.__color, self.rect.center, 8)
         for edge in self.edge_list:
             pygame.draw.line(screen, RED, self.rect.center, edge.neighbor.rect.center)
 
+    def get_id(self):
+        return self.__id
+
 
 def build_nav_graph(screen_size):
-    build_nav_graph_lists = pygame.sprite.Group()
-    divide = 40
+    build_nav_graph_group = pygame.sprite.Group()
+    nav_graph_dic = {}
+    divide = 6
     screen_width_step = math.floor(screen_size[0] / divide)
     screen_height_step = math.floor(screen_size[1] / divide)
-    array = [0] * (divide + 1)
-    for i in range(divide + 1):
-        array[i] = [0] * (divide + 1)
+    array = [0] * (divide)
+    for i in range(divide):
+        array[i] = [0] * (divide)
     xi = 0
     for x in range(screen_width_step, screen_size[0], screen_width_step):
         yi = 0
         for y in range(screen_height_step, screen_size[1], screen_height_step):
             nav_graph_node = NavGraphNode(Vector(x - (screen_width_step/2), y - (screen_height_step/2)))
-            build_nav_graph_lists.add(nav_graph_node)
+            build_nav_graph_group.add(nav_graph_node)
+            nav_graph_dic[nav_graph_node.get_id()] = nav_graph_node
             array[xi][yi] = nav_graph_node
             yi += 1
         xi += 1
@@ -76,14 +94,56 @@ def build_nav_graph(screen_size):
                 node_right = array[node_x][node_y + 1]
                 node.add_neighbor(node_right)
 
-    return build_nav_graph_lists
+    return nav_graph_dic, build_nav_graph_group
 
 
-def event_handler(running):
+def get_min(Q):
+    id = min(Q.items(), key=operator.itemgetter(1))[0]
+    del Q[id]
+    return id
+
+
+
+def dijkstras_algorithm(nav_graph, start_node, end_node):
+    d = {}
+    poprzednik = {}
+    #krok 1 dla wszystkich wierzchołków w grafie ustawiam inf jako odległość od wierzchołka startowego. dla wierzchołka startowego ustawiam 0
+    for node in nav_graph.values():
+        d[node.get_id()] = math.inf
+        poprzednik[node.get_id()] = None
+    d[start_node.get_id()] = 0
+
+    Q = d.copy()
+
+    while len(Q) > 0:
+        u_node = nav_graph[get_min(Q)]
+        u_neighbors = u_node.get_neighbors()
+        for u_neighbor in u_neighbors:
+            if d[u_neighbor.neighbor.get_id()] > d[u_node.get_id()] + u_neighbor.weight:
+                d[u_neighbor.neighbor.get_id()] = d[u_node.get_id()] + u_neighbor.weight
+                poprzednik[u_neighbor.neighbor.get_id()] = u_node.get_id()
+
+    path = []
+    step = end_node.get_id()
+    while step is not start_node.get_id():
+        x = poprzednik[step]
+        path.append(x)
+        step = x
+
+    for p in path:
+        nav_graph[p].mark_path()
+
+def event_handler(running, nav_graph_dic):
+    selected_node_id = -1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    return running
+        if event.type == pygame.MOUSEBUTTONUP:
+            cursor_pos = pygame.mouse.get_pos()
+            for key, value in nav_graph_dic.items():
+                if value.rect.collidepoint(cursor_pos):
+                    selected_node_id = key
+    return running, selected_node_id
 
 
 def main():
@@ -91,10 +151,23 @@ def main():
     screen_size = [500, 500]
     screen = pygame.display.set_mode(screen_size)
     running = True
-    nav_graph = build_nav_graph(screen_size)
+    nav_graph_dic, nav_graph_group = build_nav_graph(screen_size)
+    start_node = -1
+    end_node = -1
+    executed = False
     while running:
-        running = event_handler(running)
-        for node in nav_graph:
+        running, selected_node_id = event_handler(running, nav_graph_dic)
+        if selected_node_id != -1 and start_node == -1:
+            start_node = nav_graph_dic[selected_node_id]
+            start_node.mark()
+        elif selected_node_id != -1 and end_node == -1 and selected_node_id != start_node.get_id():
+            end_node = nav_graph_dic[selected_node_id]
+            end_node.mark()
+        if start_node != -1 and end_node != -1 and not executed :
+            dijkstras_algorithm(nav_graph_dic, start_node, end_node)
+            executed = True
+
+        for node in nav_graph_group:
             node.draw(screen)
         pygame.display.flip()
     pygame.quit()
