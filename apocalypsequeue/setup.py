a2a4c481e4a308763183f1a34be0194029b356c6
asar <asar.bs@gmail.com>
2021-02-14 19:15:13 +0100

    add setup.py file

---
+++ apocalypsequeue/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name="py_CovidApocalypse",
+    version='0.0.1',
+    author="Bartosz Skorupa",
+    author_email="bartosz@skorupa.net",
+    license="BSD",
+    keywords= "Covid Infection simulation",
+    packages=['.', 'apocalypse', 'data', 'icons', 'system']
+)
