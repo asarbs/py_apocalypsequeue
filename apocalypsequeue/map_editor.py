0462d7f698d3984086434e2d8c433de99bb75daa
asar <asar.bs@gmail.com>
2021-02-18 20:36:07 +0100

    start working on map editor

--- apocalypsequeue/map_editor.py
+++ apocalypsequeue/map_editor.py
@@ -1,7 +1,11 @@
 import pygame
 import pygame_gui
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 BACKGROUND_COLOR = (0, 0, 0)
+GREEN = (0, 255, 0)
 
 
 def main():
@@ -9,6 +13,7 @@ def main():
     clock = pygame.time.Clock()
     fps = 60
     is_running = True
+    edit_mode = False
 
     pygame.init()
     pygame.display.set_caption("Map Editor")
@@ -17,22 +22,42 @@ def main():
 
     gui_manager = pygame_gui.UIManager(size)
 
-    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)), text="Hello World", manager=gui_manager)
+    created_rectangles = []
+    counter = 0
+    tmp_rec = None
 
     while is_running:
-
+        screen.fill(BACKGROUND_COLOR)
         time_delta = clock.tick(fps) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-            if event.type == pygame.USEREVENT:
-                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                    if event.ui_element == hello_button:
-                        print("Hello World")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                edit_mode = True
+                if tmp_rec is None:
+                    counter += 1
+                    tmp_rec = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
+            if event.type == pygame.MOUSEBUTTONUP:
+                edit_mode = False
+                if tmp_rec is not None:
+                    created_rectangles.append(tmp_rec.copy())
+                tmp_rec = None
+            if edit_mode == True and event.type == pygame.MOUSEMOTION:
+                pos = pygame.mouse.get_pos()
+                height = pos[1] - tmp_rec.top
+                width = pos[0] - tmp_rec.left
+                tmp_rec.height = height
+                tmp_rec.width = width
+
             gui_manager.process_events(event)
 
+        for rect in created_rectangles:
+            pygame.draw.rect(screen, GREEN, rect)
+        if tmp_rec is not None:
+            print(u'tmp_rec.pos={}'.format((tmp_rec.top, tmp_rec.left, tmp_rec.bottom, tmp_rec.right)))
+            pygame.draw.rect(screen, GREEN, tmp_rec)
+
         gui_manager.update(time_delta)
-#        screen.blit(BACKGROUND_COLOR, (0, 0))
         gui_manager.draw_ui(screen)
 
         pygame.display.update()
