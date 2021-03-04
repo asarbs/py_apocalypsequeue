0c80ef7421759809d63847c3d9462b52390ae286
asarb <asar.bs@gmail.com>
2021-03-04 20:30:57 +0100

    remove unused measurement

--- apocalypsequeue/data/__init__.py
+++ apocalypsequeue/data/__init__.py
@@ -28,24 +28,15 @@ class Data:
 
     def addTimeData(self, time_step):
         if time_step not in self.outdata:
-            self.outdata[time_step] = {"time_step": 0, "number_of_infected": 0, "number_of_new_infected": 0,
-                                       "number_of_clients_in_queue": 0, "number_of_healthy": 0, "number_of_infection": 0}
+            self.outdata[time_step] = {"time_step": 0, "number_of_infected": 0, "number_of_new_infected": 0, "number_of_infection": 0}
 
     def addStats(self, clients_lists, time_step):
-
-        # if time_step not in self.outdata:
-        #     self.outdata[time_step] = {"time_step": 0, "number_of_infected": 0, "number_of_new_infected": 0,
-        #                           "number_of_clients_in_queue": 0, "number_of_healthy": 0}
         self.outdata[time_step]["time_step"] += 1
         for c in clients_lists:
             if c.isInfected():
                 self.outdata[time_step]["number_of_infected"] += 1
                 if not c.canInfect():
                     self.outdata[time_step]["number_of_new_infected"] += 1
-            else:
-                self.outdata[time_step]["number_of_healthy"] += 1
-            if c.standingInQueue():
-                self.outdata[time_step]["number_of_clients_in_queue"] += 1
 
     def add_infection_params(self, pos, time):
         self.infectionPos.append(pos)
@@ -72,13 +63,10 @@ class Data:
         csvfile.write(
             "time step; num_of_timesteps;number of infected; number of new infected;number of healthy; number of clients in queue; number_of_infections\n")
         for time_step in self.outdata:
-            # {"number_of_infected": 0, "number_of_new_infected": 0, "number_of_clients_in_queue": 0, "number_of_healthy": 0}
-            line = '{}|{}|{:.2f}|{:.2f}|{:.2f}|{:.2f}|{:.2f}\n'.format(time_step,
+            line = '{}|{}|{:.2f}|{:.2f}|{:.2f}\n'.format(time_step,
                                                                 self.outdata[time_step]["time_step"],
                                                                 (self.outdata[time_step]["number_of_infected"]          / self.outdata[time_step]["time_step"]),
                                                                 (self.outdata[time_step]["number_of_new_infected"]      / self.outdata[time_step]["time_step"]),
-                                                                (self.outdata[time_step]["number_of_healthy"]           / self.outdata[time_step]["time_step"]),
-                                                                (self.outdata[time_step]["number_of_clients_in_queue"]  / self.outdata[time_step]["time_step"]),
                                                                 (self.outdata[time_step]["number_of_infection"]         / self.outdata[time_step]["time_step"])
                                                                 )
             csvfile.write(line.replace(".", ","))
@@ -89,15 +77,11 @@ class Data:
             'time_step': self.outdata.keys(),
             'number_of_infected': [],
             'number_of_new_infected': [],
-            'number_of_healthy': [],
-            'number_of_clients_in_queue': [],
             'number_of_infection': []
         }
         for time_step in self.outdata:
             df_dic['number_of_infected'].append        (self.outdata[time_step]["number_of_infected"] / self.outdata[time_step]["time_step"])
             df_dic['number_of_new_infected'].append    (self.outdata[time_step]["number_of_new_infected"] / self.outdata[time_step]["time_step"])
-            df_dic['number_of_healthy'].append         (self.outdata[time_step]["number_of_healthy"] / self.outdata[time_step]["time_step"])
-            df_dic['number_of_clients_in_queue'].append(self.outdata[time_step]["number_of_clients_in_queue"] / self.outdata[time_step]["time_step"])
             df_dic['number_of_infection'].append       (self.outdata[time_step]["number_of_infection"] / self.outdata[time_step]["time_step"])
 
 
@@ -109,8 +93,6 @@ class Data:
 
         number_of_infected, = axs[0].plot('time_step', 'number_of_infected', data=df, marker='', color='#1f77b4', linewidth=1)
         number_of_new_infected, = axs[0].plot('time_step', 'number_of_new_infected', data=df, marker='', color='#ff7f0e', linewidth=1)
-        number_of_healthy, = axs[0].plot('time_step', 'number_of_healthy', data=df, marker='', color='#2ca02c', linewidth=1)
-        number_of_clients_in_queue, = axs[0].plot('time_step', 'number_of_clients_in_queue', data=df, marker='', color='#d62728', linewidth=1)
         axs[1].bar('time_step', 'number_of_infection', data=df, color='#6f1787')
 
         contact_time_values = (x / CONSOLE_ARGS.num_of_repeat_max for x in self.contact_time.values())
@@ -119,8 +101,8 @@ class Data:
         axs[0].set_xlabel("Czas [krok symulacji]")
         axs[0].set_ylabel("Liczba klientów")
         axs[0].set_title("Statysyki klientów w czasie")
-        axs[0].legend([number_of_infected, number_of_new_infected, number_of_healthy, number_of_clients_in_queue],
-                   ['Liczba wszystkich zainfekowanych', 'Liczba zainfekowanych w trakcie symulacji', 'Liczba zdrowych', 'Liczba klientów w kolejce'])
+        axs[0].legend([number_of_infected, number_of_new_infected],
+                   ['Liczba wszystkich zainfekowanych', 'Liczba zainfekowanych w trakcie symulacji'])
 
         axs[1].set_xlabel("Czas [krok symulacji]")
         axs[1].set_ylabel("Liczba infeksji")
